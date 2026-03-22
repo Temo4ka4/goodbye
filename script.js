ae91a87f06e73d9d881a59312fa515a5a1d8874f
Temo4ka4 <aberesnev490@gmail.com>
2026-03-22 18:07:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+const elements = document.querySelectorAll('.fade-in');
+elements.forEach((el, i) => {
+setTimeout(() => {
+el.classList.add('visible');
+}, i * 150);
+});
+});
